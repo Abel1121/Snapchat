683aeffcbf132d823f218ea88ae5297bf9a6470a
Marek Kobyliński <Marecki1121@gmail.com>
2019-10-08 20:05:03 +0200

    feat: list of camera with ID

--- src/js/camera.js
+++ src/js/camera.js
@@ -4,10 +4,11 @@ camera = { audio: false, video: { width: 1980, height: 1024 } };
 
 navigator.mediaDevices.getUserMedia(camera)
 .then(function(mediaStream) {
-  var video = document.querySelector('#app-video');
+  let video = document.querySelector('#app-video');
   video.srcObject = mediaStream;
   video.onloadedmetadata = function(e) {
     video.play();
   };
 })
 .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 // init camera
 import {Camera} from "./camera.js";
 
-new Camera();
+new Camera().facingMode;
+
+  navigator.mediaDevices.enumerateDevices()
+  .then(function(devices) {
+    devices.forEach(function(device) {
+        if (device.kind === "videoinput"){
+      console.log(device.kind + ": " + device.label +
+                  " id = " + device.deviceId);
+        }
+    });
+  })
+  .catch(function(err) {
+    console.log(err.name + ": " + err.message);
+  });
